92dae060dafdad94bfdea2c1d5e528890ca44bd2
wwwkang8 <wwwkang8@gmail.com>
2020-10-07 23:52:51 +0900

    #4.2 React Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {ThemeProvider} from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
+import PropTypes from "prop-types";
 
 export default () => (
 
   <ThemeProvider theme={Theme}>
-    <p>asdfad</p>
-    <GlobalStyles />
+    <>
+      <GlobalStyles />
+      <Router isLoggedIn={false} />
+    </>
   </ThemeProvider>
 
 
