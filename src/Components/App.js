9660f987e64912709bd48309918dde407bcf2529
wwwkang8 <wwwkang8@gmail.com>
2020-10-09 02:51:20 +0900

    #4.3 ApollClient

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,8 @@ import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
 
+
+
 export default () => (
 
   <ThemeProvider theme={Theme}>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
+import { ApolloProvider } from "react-apollo-hooks";
+import Client from "./Apollo/Client";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={Client}>
+        <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
